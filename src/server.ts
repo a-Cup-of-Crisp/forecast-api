36ad4dea9e9394d1c8814d2f113924b0e00b4588
Alferov Arkady <arhe414@gmail.com>
2022-12-10 00:29:25 +0300

    Weather provider added

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import { Request, Response } from "express";
+import { WeatherProvider } from "./weather-provider/weather.provider";
 
 const PORT: number = 3000;
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.json("Hello!");
+});
+
+app.get("/getWeather", async (req: Request, res: Response) => {
+  let data = await WeatherProvider.getWeather('48.480311', '135.071756');
+
+  res.send(data);
 });
 
 app.listen(PORT, () => {

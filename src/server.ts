725d3213c0d23eefdfbb1932b1ed93c7f684fdd7
Christian Guetnga <lsdrfrx@gmail.com>
2022-12-10 00:41:53 +0300

    added dockerfile and docker-compose.yaml

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import express from "express";
 import { Request, Response } from "express";
+import dotenv from "dotenv";
 
-const PORT: number = 3000;
+dotenv.config();
+
+const PORT: number = process.env.PORT ? +process.env.PORT : 3000;
+const HOST: string = process.env.HOST ? process.env.HOST : "localhost";
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.json("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Server listening on http://${HOST}:${PORT}`);
 });

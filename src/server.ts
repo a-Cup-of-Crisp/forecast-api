853f6c15322922d3759bfef07b1ec5554a1fe680
Christian Guetnga <lsdrfrx@gmail.com>
2022-12-10 01:03:15 +0300

    added middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
-import { Request, Response } from "express";
+import { Request, Response, urlencoded, json } from "express";
+import cors from "cors";
 import { WeatherProvider } from "./weather-provider/weather.provider";
 import dotenv from "dotenv";
 
@@ -9,6 +10,9 @@ const PORT: number = process.env.PORT ? +process.env.PORT : 3000;
 const HOST: string = process.env.HOST ? process.env.HOST : "localhost";
 const app = express();
 
+app.use(cors());
+app.use(urlencoded());
+app.use(json());
 app.get("/getWeather", async (req: Request, res: Response) => {
   let data = await WeatherProvider.getWeather("48.480311", "135.071756");
 
